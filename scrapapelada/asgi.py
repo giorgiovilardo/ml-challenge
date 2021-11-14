7fe7d2da38955a2d00ceeedbcc930e7fbfbaf1c6
Giorgio Vilardo <giorgio@vilardo.eu>
2021-11-14 21:26:19 +0100

    :art: blackify

--- scrapapelada/asgi.py
+++ scrapapelada/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapapelada.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scrapapelada.settings")
 
 application = get_asgi_application()
--- scrapapelada/wsgi.py
+++ scrapapelada/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scrapapelada.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "scrapapelada.settings")
 
 application = get_wsgi_application()
