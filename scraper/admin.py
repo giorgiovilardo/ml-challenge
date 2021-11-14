8a6c7bc1049fcd1c4f45dac5128f4814c1257601
Giorgio Vilardo <giorgio@vilardo.eu>
2021-11-14 21:28:03 +0100

    :sparkles: added ScraperResponse to django admin

--- scraper/admin.py
+++ scraper/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import ScraperResponse
+
+
+class ScraperResponseAdmin(admin.ModelAdmin):
+    list_display = ("url", "uuid", "has_request_failed")
+    readonly_fields = ["uuid"]
+
+
+admin.site.register(ScraperResponse, ScraperResponseAdmin)
